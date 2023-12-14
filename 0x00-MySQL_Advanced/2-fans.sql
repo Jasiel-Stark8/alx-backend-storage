d3f0acc114c22756dfa1c7cc300603d0fd4fa2f1
== <==>
2023-12-14 16:36:43 +0000

    IMPORT AND SORT FANS

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- IMPORT AND SORT FANS
--- non unique fans
+-- script that ranks country origins of bands, ordered
+-- by the number of (non-unique) fans
 
-SELECT origin, SUM(fans) AS nbfans FROM metal_bands
+SELECT origin, SUM(fans) as nb_fans from metal_bands
 GROUP BY origin ORDER BY nb_fans DESC;
