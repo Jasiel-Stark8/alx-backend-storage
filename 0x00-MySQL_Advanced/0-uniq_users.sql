15dcbf0f26e977f215ae520042e5f93098e8f71c
== <==>
2023-12-14 12:46:22 +0000

    Create users table if it doesn't exist

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,7 @@
 -- Create users table with id, email, and name
 
 CREATE TABLE IF NOT EXISTS users(
-    id INTEGER NOT NULL PRIMARY KEY,
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(225)
+    name VARCHAR(255)
 );
