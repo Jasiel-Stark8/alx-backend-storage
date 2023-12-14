a941fa0230daa8c6c8ff6574baeedc3fc76dc633
== <==>
2023-12-14 16:36:04 +0000

    IMPORT AND SORT FANS

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,7 +1,5 @@
 -- IMPORT AND SORT FANS
 -- non unique fans
 
-SELECT origin, SUM(fans) AS nbfans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nbfans FROM metal_bands
+GROUP BY origin ORDER BY nb_fans DESC;
