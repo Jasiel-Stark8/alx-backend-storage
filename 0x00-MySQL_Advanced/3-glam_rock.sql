a5b3aa728684e40f2e53da6a8308a39f158a8e5f
== <==>
2023-12-14 18:44:32 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,10 @@
--- Old school band
+-- Script to list and rank Glam rock bands by longevity
 
-SELECT band_name, (2022 - formed) AS lifespan
+SELECT band_name,
+    CASE
+        WHEN split = '0' THEN 20022 - formed
+        ELSE split - formed
+    END AS lifespan
 FROM metal_bands
 WHERE style = 'Glam rock'
-ORDER BY lifespan
+ORDER BY lifespan DESC;
