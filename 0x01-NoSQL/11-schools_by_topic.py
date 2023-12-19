790780c02663c654f3b289bea8fc6d76b5e81715
== <==>
2023-12-19 14:11:34 +0000

    returns the list of school having a specific topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """returns the list of school having a specific topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """return specific topic in school list
+    param1: mongo_collection
+    param2: topic
+    return: None or Topic
+    """
+
+    topic = mongo_collection.find({"topic": topic})
+    return topic
