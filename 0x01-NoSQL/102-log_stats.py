dbeb69f5e0bd90d8568472d4c86a7fecab707dec
== <==>
2023-12-19 16:02:52 +0000

    Log stats - Nginx logs analysis with ip

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+"""Log stats - Nginx logs analysis"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """Function to print stats about Nginx logs"""
+    client = MongoClient('mongodb://localhost:27017/')
+    db = client.logs
+    nginx_collection = db.nginx
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_count = {method: nginx_collection.count_documents({"method":\
+        method}) for method in methods}
+    total_logs = sum(method_count.values())
+    status_check_logs = nginx_collection.count_documents({"method": "GET", "path": "/status"})
+
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_count[method]}")
+    print(f"{status_check_logs} status check")
+
+    top_ips = nginx_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    print("\nIPs:")
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
