be5e079b7acc05ba767043348930e1ba2add699d
== <==>
2023-12-19 16:09:37 +0000

    Log stats - Nginx logs analysis with ip

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -3,34 +3,29 @@
 from pymongo import MongoClient
 
 
-
 def log_stats():
     """Function to print stats about Nginx logs and top IPs"""
     client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
     nginx_collection = db.nginx
 
-    # Existing code for methods and status check
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     method_count = {method: nginx_collection.count_documents({"method": method}) for method in methods}
     total_logs = sum(method_count.values())
     status_check_logs = nginx_collection.count_documents({"method": "GET", "path": "/status"})
 
-    # Code for top 10 IPs
     top_ips = nginx_collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
 
-    # Printing the stats
     print(f"{total_logs} logs")
     print("Methods:")
     for method in methods:
         print(f"\tmethod {method}: {method_count[method]}")
     print(f"{status_check_logs} status check")
 
-    # Printing top IPs
     print("IPs:")
     for ip in top_ips:
         print(f"\t{ip['_id']}: {ip['count']}")
