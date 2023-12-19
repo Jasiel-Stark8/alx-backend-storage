8ad957bde12bbdde27fffa2cb2d618a0d1581455
== <==>
2023-12-19 15:10:08 +0000

    Log all stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,29 +1,23 @@
 #!/usr/bin/env python3
-"""Log stats"""
+"""Log stats - Nginx logs analysis"""
 from pymongo import MongoClient
 
-
 def log_stats():
-    """Show logs
-    param1: database name
-    param2: collection name
-    return: status | all logs OR None
-    """
+    """Function to print stats about Nginx logs"""
     client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
     nginx_collection = db.nginx
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     method_count = {method: nginx_collection.count_documents({"method": method}) for method in methods}
+    total_logs = sum(method_count.values())
+    status_check_logs = nginx_collection.count_documents({"method": "GET", "path": "/status"})
 
-    log_total = sum(method_count.values())
-    check_log_status = nginx_collection.count_documents({"method": "GET", "path": "/status"})
-
-    print(f"{log_total} logs")
+    print(f"{total_logs} logs")
     print("Methods:")
     for method in methods:
-        print(f"method {method}: {method_count[method]}")
-    print(f"{check_log_status} status check")
+        print(f"\tmethod {method}: {method_count[method]}")
+    print(f"{status_check_logs} status check")
 
 if __name__ == "__main__":
     log_stats()
