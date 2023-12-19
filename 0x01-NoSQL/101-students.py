37e50e343e7811f073112ab673f7b6d0e80f02f0
== <==>
2023-12-19 15:47:47 +0000

    return all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -2,23 +2,22 @@
 """Top students"""
 
 
-def top_students(mongo_collection, score):
-    """return all students sorted by average score"""
+def top_students(mongo_collection):
+    """Return all students sorted by average score"""
     pipeline = [
         {
-            "$unwind": "scores"
+            "$unwind": "$scores"
         },
         {
             "$group": {
                 "_id": "$_id",
                 "name": {"$first": "$name"},
-                "scores": "{$avg: $scores.score}"
+                "averageScore": {"$avg": "$scores.score"}
             }
         },
         {
             "$sort": {"averageScore": -1}
         }
     ]
-
     top = mongo_collection.aggregate(pipeline)
     return list(top)
