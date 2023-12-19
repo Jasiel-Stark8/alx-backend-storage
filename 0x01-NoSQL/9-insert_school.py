6fef494e3250ca6522f2ede384289a611824b046
== <==>
2023-12-19 14:00:48 +0000

    Insert a document

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """Insert a document in Python"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a document
+    param1: mongo_collection 
+    param2: **kwargs
+    return: None
+    """
+
+    documents = mongo_collection.insert({**kwargs})
+    return documents
