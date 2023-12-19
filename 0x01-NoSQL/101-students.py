74a47bad7382ce0fec304c19b673c5128f8c1c12
== <==>
2023-12-19 15:38:33 +0000

    return all students sorted by average score

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Top students"""
+
+
+def top_students(mongo_collection):
+    """return all students sorted by average score"""
+    top = mongo_collection.find({"$avg": {"score": score}})
+    return top
