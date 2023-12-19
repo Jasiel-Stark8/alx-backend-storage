7beb2de07cd8102b923197d48024d6b2ab99072a
== <==>
2023-12-19 16:04:14 +0000

    Log stats - Nginx logs analysis with ip

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python3
-"""Log stats - Nginx logs analysis"""
+"""Log stats - Nginx logs analysis with top IPs"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Function to print stats about Nginx logs"""
+    """Function to print stats about Nginx logs and top IPs"""
     client = MongoClient('mongodb://localhost:27017/')
     db = client.logs
     nginx_collection = db.nginx
 
+    # Existing code for methods and status check
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_count = {method: nginx_collection.count_documents({"method":\
-        method}) for method in methods}
+    method_count = {method: nginx_collection.count_documents({"method": method}) for method in methods}
     total_logs = sum(method_count.values())
     status_check_logs = nginx_collection.count_documents({"method": "GET", "path": "/status"})
 
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method in methods:
-        print(f"\tmethod {method}: {method_count[method]}")
-    print(f"{status_check_logs} status check")
-
+    # New code for top 10 IPs
     top_ips = nginx_collection.aggregate([
         {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
         {"$sort": {"count": -1}},
         {"$limit": 10}
     ])
 
-    print("\nIPs:")
-    for ip in top_ips:
-        print(f"\t{ip['_id']}: {ip['count']}")
+    # Printing the existing stats
+    print(f"{total_logs} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_count[method]}")
+    print(f"{status_check_logs} status check")
 
+    # Printing top IPs
+    print("\nTop 10 IPs:")
+    for ip in top_ips:
+        print(f"{ip['_id']}: {ip['count']}")
 
 if __name__ == "__main__":
     log_stats()
