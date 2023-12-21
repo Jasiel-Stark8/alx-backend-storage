8c3b1af4977a579c4d1c7939c4dd1cbee187849e
== <==>
2023-12-21 01:38:34 +0000

    Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+import redis
+import uuid
+from typing import Union
+
+class Cache:
+    """Parent Cache Class"""
+
+    def __init__(self):
+        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, int]) -> str:
+        if data:
+            key = str(uuid.uuid4())
+            self._redis.set(key, data)
+            return key
+        return ""
