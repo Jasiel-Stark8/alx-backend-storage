50615218bbe21509a62bb99995bbf8fdadd09e41
== <==>
2023-12-21 01:56:27 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,10 +10,12 @@ class Cache:
     """Parent Cache Class"""
 
     def __init__(self):
+        """Initialize redis connection and flush db at start"""
         self._redis = redis.Redis(host='localhost', port=6379)
         self._redis.flushdb()
 
     def store(self, data: Union[str, int, bytes, float]) -> str:
+        """Generate uuid and store string data"""
         if data:
             key = str(uuid.uuid4())
             self._redis.set(key, data)
