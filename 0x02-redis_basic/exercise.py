603b3ccc3d75fd3982e0efd01f51102db922a7b5
== <==>
2023-12-21 01:51:26 +0000

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,9 +10,11 @@ class Cache:
     """Parent Cache Class"""
 
     def __init__(self):
-        self._redis = redis.Redis(host='localhost', port=6379)
+        self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
+    @call_history
     def store(self, data: Union[str, int]) -> str:
         if data:
             key = str(uuid.uuid4())
